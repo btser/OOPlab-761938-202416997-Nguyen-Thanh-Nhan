41934bff91840d6baabb1b3f8716004c14af42a9
OOPlab - 761938 - 202416997 - Nguyễn Thành Nhân <vthanhnhan2k6@gmail.com>
2026-04-04 18:44:39 +0700

    remove disc in the cart

--- Lab02/src/AIMS.java
+++ Lab02/src/AIMS.java
@@ -1,8 +1,8 @@
 public class AIMS {
 
-    public static void main(String[] args)
+    public static void main(String[] args)                              
     {
-        Cart newOrder = new Cart();
+        Cart newOrder = new Cart();         
         DigitalVideoDisc disc_1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         newOrder.addDigitalVideoDisc(disc_1);
         DigitalVideoDisc disc_2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Geogre Lucas", 87, 24.95f);
@@ -10,6 +10,10 @@ public class AIMS {
         DigitalVideoDisc disc_3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         newOrder.addDigitalVideoDisc(disc_3);
         System.out.print("Total cost is: ");
+        System.out.println(newOrder.totalCost());
+
+        newOrder.removeDigitalVideoDisc(disc_3);                           // remove disc Aladin
+        System.out.print("Total cost after remove disc 3 is: ");        //  print total cost after remove disc Aladin
         System.out.println(newOrder.totalCost());
     }
 }
