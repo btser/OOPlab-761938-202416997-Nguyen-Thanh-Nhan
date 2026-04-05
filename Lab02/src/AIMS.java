8d27137313dbe04308860ae72430c9c04ecb5740
OOPlab - 761938 - 202416997 - Nguyễn Thành Nhân <vthanhnhan2k6@gmail.com>
2026-04-05 18:15:48 +0700

    Section 14.1: Overloading using array

--- Lab02/src/AIMS.java
+++ Lab02/src/AIMS.java
@@ -4,11 +4,14 @@ public class AIMS {
     {
         Cart newOrder = new Cart();         
         DigitalVideoDisc disc_1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        newOrder.addDigitalVideoDisc(disc_1);
+
         DigitalVideoDisc disc_2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Geogre Lucas", 87, 24.95f);
-        newOrder.addDigitalVideoDisc(disc_2);
+
         DigitalVideoDisc disc_3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        newOrder.addDigitalVideoDisc(disc_3);
+
+        DigitalVideoDisc[] dvdList = {disc_1, disc_2, disc_3};
+        newOrder.addDigitalVideoDisc(dvdList);
+
         System.out.print("Total cost is: ");
         System.out.println(newOrder.totalCost());
 
--- Lab02/src/Cart.java
+++ Lab02/src/Cart.java
@@ -2,16 +2,21 @@ public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered;
-    public void addDigitalVideoDisc(DigitalVideoDisc disc)
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList)
     {
-        if (qtyOrdered < MAX_NUMBERS_ORDERED)
+        for (int i = 0; i < dvdList.length; i++)
         {
-            itemsOrdered[qtyOrdered] = disc;
-            qtyOrdered++;
-            System.out.println("The disc has been added!");
-        }else
-        {
-            System.out.println("The cart is almost full!");
+            DigitalVideoDisc disc = dvdList[i];
+            if (qtyOrdered < MAX_NUMBERS_ORDERED)
+            {
+                itemsOrdered[qtyOrdered] = disc;
+                qtyOrdered++;
+                System.out.println("The disc " + disc.getTitle() + "has been added!");
+            }else
+            {
+                System.out.println("The cart is full!");
+                break;
+            }
         }
     }
 
